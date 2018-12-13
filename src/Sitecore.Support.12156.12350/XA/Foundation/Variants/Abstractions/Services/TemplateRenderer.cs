a43d3491652ed73915a7f135125a5cb3c641d84e
DK\psk <psk@sitecore.net>
2018-12-13 17:42:03 +0200

    fix 12156

--- src/Sitecore.Support.12156.12350/XA/Foundation/Variants/Abstractions/Services/TemplateRenderer.cs
+++ src/Sitecore.Support.12156.12350/XA/Foundation/Variants/Abstractions/Services/TemplateRenderer.cs
@@ -32,9 +32,12 @@ namespace Sitecore.Support.XA.Foundation.Variants.Abstractions.Services
           {
             _context.Put(parameter.Key, parameter.Value);
           }
-          StringWriter output = new StringWriter();
-          Velocity.Evaluate(_context, output, templateName, template);
-          return output.GetStringBuilder().ToString();
+
+          using (StringWriter output = new StringWriter())
+          {
+            Velocity.Evaluate(_context, output, templateName, template);
+            return output.GetStringBuilder().ToString();
+          }
         }
       }
       catch (Exception ex)
